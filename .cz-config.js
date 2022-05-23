a5da532cb052479687a5f1f924f9dab87919cf9c
liudongjun <3170495286@qq.com>
2022-05-23 10:53:20 +0800

    fix:添加规则

--- .cz-config.js
+++ .cz-config.js
@@ -1,28 +1,28 @@
 module.exports = {
   types: [
-    { value: "feat", name: "feat: 添加新特性" },
-    { value: "fix", name: "fix: 修复bug" },
-    { value: "docs", name: "docs: 仅仅修改了文档" },
+    { value: 'feat', name: 'feat: 添加新特性' },
+    { value: 'fix', name: 'fix: 修复bug' },
+    { value: 'docs', name: 'docs: 仅仅修改了文档' },
     {
-      value: "style",
-      name: "style: 仅仅修改了空格、格式缩进、都好等等，不改变代码逻辑",
+      value: 'style',
+      name: 'style: 仅仅修改了空格、格式缩进、都好等等，不改变代码逻辑'
     },
-    { value: "refactor", name: "refactor: 代码重构，没有加新功能或者修复bug" },
-    { value: "perf", name: "perf: 增加代码进行性能测试" },
-    { value: "test", name: "test: 增加测试用例" },
-    { value: "chore", name: "chore: 改变构建流程、或者增加依赖库、工具等" },
+    { value: 'refactor', name: 'refactor: 代码重构，没有加新功能或者修复bug' },
+    { value: 'perf', name: 'perf: 增加代码进行性能测试' },
+    { value: 'test', name: 'test: 增加测试用例' },
+    { value: 'chore', name: 'chore: 改变构建流程、或者增加依赖库、工具等' }
   ],
   messages: {
-    type: "请选择提交类型:",
+    type: '请选择提交类型:',
     // customScope:'请输入修改范围(可选):',
-    customScope: "按规范不需要填写直接回车跳过(Enter):",
-    subject: "请简要描述提交(必填):",
-    body: "请输入详细描述(可选):",
-    footer: "请输入要关闭的issue(可选):",
-    confirmCommit: "确认要使用以上信息提交? (y/n)",
+    customScope: '按规范不需要填写直接回车跳过(Enter):',
+    subject: '请简要描述提交(必填):',
+    body: '请输入详细描述(可选):',
+    footer: '请输入要关闭的issue(可选):',
+    confirmCommit: '确认要使用以上信息提交? (y/n)'
   },
   // 跳过步骤
-  skipQuestions: ["body", "footer"],
+  skipQuestions: ['body', 'footer'],
   // 默认长度72
-  subjectLimit: 72,
-};
+  subjectLimit: 72
+}
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,30 @@
-import { createVuePlugin } from "vite-plugin-vue2";
+import { defineConfig } from 'vite'
+import { createVuePlugin } from 'vite-plugin-vue2'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
-export default {
+export default defineConfig({
   plugins: [createVuePlugin()],
-};
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'), // 设置 `@` 指向 `src` 目录
+      '@api': resolve(__dirname, 'src/api'),
+      '@comp': resolve(__dirname, 'src/components'),
+      '@util': resolve(__dirname, 'src/utils'),
+      '@sty': resolve(__dirname, 'src/style')
+    }
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 8081, // 设置服务启动端口号
+    open: false // 设置服务启动时是否自动打开浏览器
+    // 代理
+    // proxy: {
+    //   "/api": {
+    //     target: "http://localhost:9000",
+    //     changeOrigin: true,
+    //     rewrite: (path) => path.replace(/^\/api/, ""),
+    //   },
+    // },
+  }
+})
